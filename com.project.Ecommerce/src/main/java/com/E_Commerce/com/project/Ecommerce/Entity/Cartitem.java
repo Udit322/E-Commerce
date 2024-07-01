d818fdb3c3152aacc6f1a4716d4dddf6d318758a
saurabhsaini <160897917+saurabh80001@users.noreply.github.com>
2024-07-01 21:37:55 +0530

    Update Cartitem.java

--- com.project.Ecommerce/src/main/java/com/E_Commerce/com/project/Ecommerce/Entity/Cartitem.java
+++ com.project.Ecommerce/src/main/java/com/E_Commerce/com/project/Ecommerce/Entity/Cartitem.java
@@ -1,5 +1,50 @@
 package com.E_Commerce.com.project.Ecommerce.Entity;
 
-public class Cartitem {
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name="cart-items-details")
+public class Cartitem {
+	private Integer cartItemId;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Poduct product;
+
+    private Integer cartItemQuantity;
+
+	public Integer getCartItemId() {
+		return cartItemId;
+	}
+
+	public void setCartItemId(Integer cartItemId) {
+		this.cartItemId = cartItemId;
+	}
+
+	public Poduct getProduct() {
+		return product;
+	}
+
+	public void setProduct(Poduct product) {
+		this.product = product;
+	}
+
+	public Integer getCartItemQuantity() {
+		return cartItemQuantity;
+	}
+
+	public void setCartItemQuantity(Integer cartItemQuantity) {
+		this.cartItemQuantity = cartItemQuantity;
+	}
+
+	public Cartitem() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+    
+    
+    
 }

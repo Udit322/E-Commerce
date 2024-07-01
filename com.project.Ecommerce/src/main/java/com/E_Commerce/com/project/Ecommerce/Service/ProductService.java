9dbd8b89e479803ec1e8e06297e5f4c436912291
Sachinjain3100 <sj8692769@gmail.com>
2024-07-01 21:09:49 +0530

    Update ProductService.java

--- com.project.Ecommerce/src/main/java/com/E_Commerce/com/project/Ecommerce/Service/ProductService.java
+++ com.project.Ecommerce/src/main/java/com/E_Commerce/com/project/Ecommerce/Service/ProductService.java
@@ -1,5 +1,12 @@
-package com.E_Commerce.com.project.Ecommerce.Service;
+package com.example.ecommerce.service;
+
+import java.util.List;
+import com.example.ecommerce.entity.Product;
 
 public interface ProductService {
-
+    List<Product> getAllProducts();
+    Product getProductById(Integer id);
+    Product addProduct(Product product);
+    Product updateProduct(Integer id, Product product);
+    void deleteProduct(Integer id);
 }

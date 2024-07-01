8b3b6f187c5c203f15696b0085c075ad0c4e0abe
saurabhsaini <160897917+saurabh80001@users.noreply.github.com>
2024-07-01 21:40:25 +0530

    Update Poduct.java

--- com.project.Ecommerce/src/main/java/com/E_Commerce/com/project/Ecommerce/Entity/Poduct.java
+++ com.project.Ecommerce/src/main/java/com/E_Commerce/com/project/Ecommerce/Entity/Poduct.java
@@ -1,5 +1,112 @@
 package com.E_Commerce.com.project.Ecommerce.Entity;
 
+import java.util.Locale.Category;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.transaction.Status;
+
+@Entity
+@Table(name= "productdetails")
 public class Poduct {
+
+private Integer productId;
+private String productName;
+private Double price;
+private String description;
+private String manufacturer;
+private Integer quantity;
+@Enumerated(EnumType.STRING)
+private Category category;
+@Enumerated(EnumType.STRING)
+private Status status;
+
+@ManyToOne
+@JoinColumn(name = "seller_id")
+private Seller seller;
+
+public Integer getProductId() {
+	return productId;
+}
+
+public void setProductId(Integer productId) {
+	this.productId = productId;
+}
+
+public String getProductName() {
+	return productName;
+}
+
+public void setProductName(String productName) {
+	this.productName = productName;
+}
+
+public Double getPrice() {
+	return price;
+}
+
+public void setPrice(Double price) {
+	this.price = price;
+}
+
+public String getDescription() {
+	return description;
+}
+
+public void setDescription(String description) {
+	this.description = description;
+}
+
+public String getManufacturer() {
+	return manufacturer;
+}
+
+public void setManufacturer(String manufacturer) {
+	this.manufacturer = manufacturer;
+}
+
+public Integer getQuantity() {
+	return quantity;
+}
+
+public void setQuantity(Integer quantity) {
+	this.quantity = quantity;
+}
+
+public Category getCategory() {
+	return category;
+}
+
+public void setCategory(Category category) {
+	this.category = category;
+}
+
+public Status getStatus() {
+	return status;
+}
+
+public void setStatus(Status status) {
+	this.status = status;
+}
+
+public Seller getSeller() {
+	return seller;
+}
+
+public void setSeller(Seller seller) {
+	this.seller = seller;
+}
+
+public Poduct() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+
+
+
 
 }

982febda07b006992a57da98a81b9b5d666f2c8b
saurabhsaini <160897917+saurabh80001@users.noreply.github.com>
2024-07-02 12:49:56 +0530

    Update CartItemRepository.java

--- com.project.Ecommerce/src/main/java/com/E_Commerce/com/project/Ecommerce/Repository/CartItemRepository.java
+++ com.project.Ecommerce/src/main/java/com/E_Commerce/com/project/Ecommerce/Repository/CartItemRepository.java
@@ -1,5 +1,7 @@
 package com.E_Commerce.com.project.Ecommerce.Repository;
 
-public interface CartItemRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.E_Commerce.com.project.Ecommerce.Entity.Cartitem;
 
+public interface CartItemRepository extends JpaRepository<Cartitem, Integer> {
 }
